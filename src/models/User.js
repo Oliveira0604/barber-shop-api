04a72f91173f8715c9b94a890b4908968551c0b4
oliveira <oliveiranathan853@gmail.com>
2026-02-10 07:48:36 -0300

    feat: add User model

--- src/models/User.js
+++ src/models/User.js
@@ -4,6 +4,12 @@ import { Schema } from 'mongoose';
 const User = mongoose.model(
     'User',
     new Schema({
-        
-    })
+        name: {type: String, required: true},
+        email: {type: String, requeired: true},
+        cellphone: {type: String, required: true},
+        password: {type: String, required: true},
+        isAdmin: {type: Boolean, default: false}
+    }, {timestamps: true})
 )
+
+export default User
